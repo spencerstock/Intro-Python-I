4b24a0c4f2fc60ec8910f30dd6514c40ab57f886
Spencer Stock <spencerstock@gmail.com>
2019-09-05 09:47:09 -0600

    Finished all nodes

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,6 +9,9 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+with open('foo.txt') as f:
+    print(str(f.read()))
+    f.close
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -16,3 +19,12 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+with open("bar.txt","w") as f:
+    f.write("My name is Barney. \n")
+    f.write("My friends call me Marney. \n")
+    f.write("But you can call me Ted.")
+    f.close
+
+with open('bar.txt') as f:
+    print(str(f.read()))
+    f.close
